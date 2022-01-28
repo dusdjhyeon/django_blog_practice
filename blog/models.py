2058017428c6a58e7b82a166bef817f011363c9e
unknown <dusdj@khu.ac.kr>
2022-01-28 19:45:05 +0900

    author 필드 추가하기

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 
@@ -13,11 +14,12 @@ class Post(models.Model):
     
     created_at = models.DateTimeField(auto_now_add=True)       #처음 레코드가 생성될 때 현재 시각이 자동으로 저장됨
     updated_at = models.DateTimeField(auto_now=True)           #다시 저장할 때마다 그 시각이 저장됨
-    #author: 추후 작성 예정
-
+    
+    #author
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #이 포스트의 작성자가 데이터베이스에서 삭제되었을 때 이 포스트도 같이 삭제한다.
 
     def __str__(self):
-        return f'[{self.pk}]{self.title}'   #pk: 각 레코드에 대한 고유값. 첫 번째 포스트는 pk값이 1, 두 번째 포스트는 pk값이 2..
+        return f'[{self.pk}]{self.title} :: {self.author}'   #pk: 각 레코드에 대한 고유값. 첫 번째 포스트는 pk값이 1, 두 번째 포스트는 pk값이 2., 작성자 정보도 출력
 
     
     def get_absolute_url(self):
